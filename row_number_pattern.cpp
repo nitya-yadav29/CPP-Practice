dbe46c9ba42c98f19694b8a749ff85e42f7a53da
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-15 04:02:39 +0530

    Create row_number_pattern.cpp

---
+++ row_number_pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p, q;
+    for(p=1;p<=5;p++)
+    {
+        for(q=1;q<=p;q++)
+        {
+            cout<<p<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
